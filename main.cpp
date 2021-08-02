f94bc8952e7d7cd55f31ead361d5518f2b31fdf7
shambu <shambuqwerty@gmail.com>
2021-08-02 23:34:22 +0530

    Template #1 - Printing a std::vector

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//--------------- Printing a std::vector ----------------//
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec){
+    os << "{ ";
+    for(auto i: vec){
+        os << i << " ";
+    }
+    return os << "}";
+}
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& vec){
+    os << "{ " << endl;
+    for(auto i: vec){
+        os << i << endl;
+    }
+    return os << "}";
+}
+
+//------------------------------------------------------//
