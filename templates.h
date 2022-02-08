44bdd8f93ba970d9060f567a9048e28f3f71cf11
Shambu Valasang <shambuqwerty@gmail.com>
2022-02-09 02:42:05 +0530

    Update templates.h

--- templates.h
+++ templates.h
@@ -4,22 +4,33 @@
 #define SIZE_TYPE int
 #endif
 
-//--------------- Printing a std::vector | tags - printing, vector, print----------------//
+#ifndef STORAGE_TYPE
+#define STORAGE_TYPE int
+#endif
+
+//-----------------------------Printing a std::vector-------------------------------------//
 
 template <class T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec){
-	os << "[";
-	for (SIZE_TYPE i = 0; i <= (SIZE_TYPE)vec.size() - 2; i++)
-		os << vec[i] << ",";
-	return os << vec.back() << "]";
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    os << "[";
+    for (SIZE_TYPE i = 0; i <= (SIZE_TYPE)vec.size() - 2; i++)
+        os << vec[i] << ",";
+    return os << vec.back() << "]";
 }
 
 template <class T>
-std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& vec){
-	os << "[ " << vec[0] << ",\n";
-	for (SIZE_TYPE i = 1; i <= (SIZE_TYPE)vec.size() - 2; i++)
-		os << "  " << vec[i] << ",\n";
-	return os << "  " << vec.back() << " ]";
+std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& vec) {
+    os << "[ " << vec[0] << ",\n";
+    for (SIZE_TYPE i = 1; i <= (SIZE_TYPE)vec.size() - 2; i++)
+        os << "  " << vec[i] << ",\n";
+    return os << "  " << vec.back() << " ]";
 }
+
+//----------------------------------------------------------------------------------------//
+//----------------------shortcut for vector.begin() and vector.end()----------------------//
+
+#ifndef all
+#define all(s) s.begin(), s.end()
+#endif
 
 //----------------------------------------------------------------------------------------//
