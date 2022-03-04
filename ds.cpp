53014a17a2996aec4873ee10457f9b93d36f79e7
shambu <shambuqwerty@gmail.com>
2022-03-04 19:50:33 +0530

    AVL tree - #1 self balancing trees

--- ds.cpp
+++ ds.cpp
@@ -231,6 +231,7 @@ namespace avl_tree {
 	/*
 		Time Complexity:
 			Insertion - O(logn)
+			Deletion - O(logn)
 			balancing - O(1)
 	*/
 	struct Node {
@@ -241,17 +242,19 @@ namespace avl_tree {
 
 		Node(int _val) {
 			val = _val;
-			height = 0;
+			height = 1;
+			left = NULL;
+			right = NULL;
 		}
 	};
 
 	int get_height(Node* root) {
-		if(not root) return 0;
+		if(root == NULL) return 0;
 		return root->height;
 	}
 
 	int get_diff(Node* root) {
-		if(not root) return 0;
+		if(root == NULL) return 0;
 		return get_height(root->left) - get_height(root->right);
 	}
 
@@ -278,12 +281,15 @@ namespace avl_tree {
 	}
 
 	Node* insert(Node* root, int val) {
-		if(not root)
+		if(root == NULL)
 			root = new Node(val);
+
 		else if(val > root->val)
 			root->right = insert(root->right, val);
-		else
+		else if(val < root->val)
 			root->left = insert(root->left, val);
+		else
+			return root;
 
 		root->height = 1 + max(get_height(root->left), get_height(root->right));
 		int diff = get_diff(root);
@@ -305,30 +311,97 @@ namespace avl_tree {
 		return root;
 	}
 
+	Node* del(Node* root, int val) {
+		if(root == NULL)
+			return root;
+
+		else if(val > root->val)
+			root->right = del(root->right, val);
+
+		else if(val < root->val)
+			root->left = del(root->left, val);
+
+		else {
+			if(root->left == NULL) {
+				Node* tmp = root->right;
+				delete root;
+				return tmp;
+			}
+			if(root->right == NULL) {
+				Node* tmp = root->left;
+				delete root;
+				return tmp;
+			}
+
+			Node* succ_parent = root;
+			Node* succ = root->right;
+
+			while(succ->left != NULL) {
+				succ_parent = succ;
+				succ = succ->left;
+			}
+
+			root->val = succ->val;
+			if(succ_parent != root)
+				succ_parent->left = succ->right;
+			else
+				succ_parent->right = succ->right;
+
+			delete succ;
+		}
+
+		if(root == NULL) return root;
+
+		root->height = 1 + max(get_height(root->left), get_height(root->right));
+
+		int diff = get_diff(root);
+
+		if(diff > 1 and get_diff(root->left) >= 0) return right_rotate(root);
+		if(diff < -1 and get_diff(root->right) <= 0) return left_rotate(root);
+
+		if(diff > 1 and get_diff(root->left) < 0) {
+			root->left = left_rotate(root->left);
+			return right_rotate(root);
+		}
+
+		if(diff < -1 and get_diff(root->right) > 0) {
+			root->right = right_rotate(root->right);
+			return left_rotate(root);
+		}
+
+		return root;
+	}
+
 	void pre_order(Node* root) {
-		if(not root) return;
+		if(root == NULL) return;
 		cout << root->val << " ";
 		pre_order(root->left);
 		pre_order(root->right);
 	}
 
 	void sample() {
-		int n;
+		int n, tmp;
 		vector<int> arr;
 
 		cin >> n;
 		while(n--) {
-			arr.push_back(int());
-			cin >> arr.back();
+			cin >> tmp;
+			arr.push_back(tmp);
 		}
 
 		avl_tree::Node* root = NULL;
 
-		for(auto i : arr) {
-			root = avl_tree::insert(root, i);
-		}
+		for(int i : arr) root = avl_tree::insert(root, i);
 
-		cout << arr << endl;
+		cout << "Original Array: " << endl;
+		cout << arr << endl << endl;
+
+		cout << "Tree (pre order)" << endl;
+		avl_tree::pre_order(root);
+		cout << endl << endl;
+
+		cout << "Tree (pre order) After Deletion(10)" << endl;
+		root = avl_tree::del(root, 10);
 		avl_tree::pre_order(root);
 		cout << endl;
 	}
